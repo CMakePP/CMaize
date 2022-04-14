d191ef8a05c785b3c6724ae065a38345d5834120
zachcran <zachcran@gmail.com>
2022-04-14 15:53:08 -0500

    Move tests into 'cpp' dir and fix syntax

--- tests/cpp/targets/cxx_library.cmake
+++ tests/cpp/targets/cxx_library.cmake
@@ -1,12 +1,16 @@
 include(cmake_test/cmake_test)
 
-ct_add_test("cpp_add_cxx_library")
+ct_add_test(NAME "cpp_add_cxx_library")
+function("${cpp_add_cxx_library}")
+
     include(cpp/targets/cxx_library)
 
     set(source_dir "${CMAKE_CURRENT_BINARY_DIR}/a_cxx_library/src")
     set(include_dir "${CMAKE_CURRENT_BINARY_DIR}/a_cxx_library/include")
 
-    ct_add_section("Normal library")
+    ct_add_section(NAME "normal_library")
+    function("${normal_library}")
+
         file(WRITE "${source_dir}/a.cpp" "int test_fxn() { return 0; }")
         file(WRITE "${include_dir}/a.hpp" "#pragma once\nint test_fxn();")
 
@@ -16,5 +20,7 @@ ct_add_test("cpp_add_cxx_library")
             INCLUDE_DIR "${include_dir}"
         )
         message(FATAL_ERROR "testing")
-    ct_end_section()
-ct_end_test()
+
+    endfunction()
+
+endfunction()
--- tests/cpp/user_api/find_or_build_dependency.cmake
+++ tests/cpp/user_api/find_or_build_dependency.cmake
@@ -1,7 +1,8 @@
 include(cmake_test/cmake_test)
 
-ct_add_test("cpp_find_or_build_dependency")
+ct_add_test("test_cpp_find_or_build_dependency")
+function("${test_cpp_find_or_build_dependency}")
     include(cpp/user_api/find_or_build_dependency)
 
     cpp_find_or_build_dependency(Catch2 URL https://github.com/catchorg/Catch2)
-ct_end_test()
+endfunction()
