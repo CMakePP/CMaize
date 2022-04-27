3528115fecf779c2d5766b0fe98ae43e17d08474
zachcran <zachcran@gmail.com>
2022-04-27 16:12:43 -0500

    Changed to write toolchain file for users to load

--- tests/cmaize/toolchain/test_toolchain.cmake
+++ tests/cmaize/toolchain/test_toolchain.cmake
@@ -10,8 +10,8 @@ function("${test_toolchain}")
     #[[[
     # Test that the constructor is working with different arguments.
     #]]
-    ct_add_section(NAME "constructor")
-    function("${constructor}")
+    ct_add_section(NAME "test_constructor")
+    function("${test_constructor}")
 
         #[[[
         # Test when no toolchain path or content argument is provided.
@@ -24,21 +24,10 @@ function("${test_toolchain}")
             # Create a toolchain object with no user options
             Toolchain(CTOR toolchain_obj)
 
-            # The file path should be blank
-            Toolchain(GET "${toolchain_obj}" my_file_path file_path)
-            ct_assert_equal(my_file_path "")
+            # The file content should be blank
+            Toolchain(GET "${toolchain_obj}" file_contents encoded_toolchain_contents)
+            ct_assert_equal(file_contents "")
 
-            # Get the map of autopopulated settings
-            Toolchain(GET "${toolchain_obj}" auto_pop_map auto_options)
-            cpp_serialize(auto_pop_map_serialized "${auto_pop_map}")
-            message(">>> Autopopulated map: ${auto_pop_map_serialized}")
-
-            # Toolchain map should be equal to autopopulated options
-            Toolchain(GET "${toolchain_obj}" toolchain_map toolchain_options)
-            cpp_serialize(toolchain_map_serialized "${toolchain_map}")
-            message(">>> Toolchain map: ${toolchain_map_serialized}")
-            ct_assert_equal(auto_pop_map_serialized "${toolchain_map_serialized}")
-        
         endfunction()
 
         #[[[
@@ -54,193 +43,20 @@ function("${test_toolchain}")
             # Create a toolchain object with a blank string
             Toolchain(CTOR toolchain_obj "")
 
-            # The file path should be blank
-            Toolchain(GET "${toolchain_obj}" my_file_path file_path)
-            ct_assert_equal(my_file_path "")
+            # The file content should be blank
+            Toolchain(GET "${toolchain_obj}" file_contents encoded_toolchain_contents)
+            ct_assert_equal(file_contents "")
 
-            # Get the map of autopopulated settings
-            Toolchain(GET "${toolchain_obj}" auto_pop_map auto_options)
-            cpp_serialize(auto_pop_map_serialized "${auto_pop_map}")
-            message(">>> Autopopulated map: ${auto_pop_map_serialized}")
-
-            # Toolchain map should be equal to autopopulated options
-            Toolchain(GET "${toolchain_obj}" toolchain_map toolchain_options)
-            cpp_serialize(toolchain_map_serialized "${toolchain_map}")
-            message(">>> Toolchain map: ${toolchain_map_serialized}")
-            ct_assert_equal(auto_pop_map_serialized "${toolchain_map_serialized}")
-        
         endfunction()
 
         #[[[
         # Test that the contents of a valid toolchain file can be provided
         # and the object is populated properly.
         #]]
-        ct_add_section(NAME "constructor_valid_toolchain")
-        function("${constructor_valid_toolchain}")
+        ct_add_section(NAME "valid_toolchain")
+        function("${valid_toolchain}")
 
-            #[[[
-            # Tests that a ``set()`` command is parsed properly.
-            #]]
-            ct_add_section(NAME "set_cmd")
-            function("${set_cmd}")
-
-                ct_add_section(NAME "single_line")
-                function("${single_line}")
-
-                    set(toolchain_contents "set(CMAKE_C_COMPILER test_compiler)")
-
-                    set(corr "test_compiler")
-
-                    # Create a toolchain object
-                    Toolchain(CTOR toolchain_obj "${toolchain_contents}")
-
-                    # Test that the toolchain map key changed accordingly
-                    Toolchain(GET "${toolchain_obj}" toolchain_map toolchain_options)
-                    cpp_map(GET "${toolchain_map}" value "CMAKE_C_COMPILER")
-                    message(">>> CMAKE_C_COMPILER: ${value}")
-                    ct_assert_equal(corr "${value}")
-
-                endfunction()
-
-                ct_add_section(NAME "multi_line")
-                function("${multi_line}")
-
-                    set(toolchain_contents "set(\nCMAKE_C_COMPILER \ntest_compiler\n)")
-
-                    set(corr "test_compiler")
-
-                    # Create a toolchain object
-                    Toolchain(CTOR toolchain_obj "${toolchain_contents}")
-
-                    # Test that the toolchain map key changed accordingly
-                    Toolchain(GET "${toolchain_obj}" toolchain_map toolchain_options)
-                    cpp_map(GET "${toolchain_map}" value "CMAKE_C_COMPILER")
-                    message(">>> CMAKE_C_COMPILER: ${value}")
-                    ct_assert_equal(corr "${value}")
-                    
-                endfunction()
-            
-            endfunction()
-
-            #[[[
-            # Tests that a ``list(APPEND`` command is parsed properly.
-            #]]
-            ct_add_section(NAME "list_append_cmd")
-            function("${list_append_cmd}")
-
-                set(toolchain_contents "list(APPEND test_prefix_path /usr/lib)\n")
-                string(APPEND toolchain_contents "list(APPEND test_prefix_path /usr/bin)")
-                
-                set(corr "/usr/lib;/usr/bin")
-
-                # Create a toolchain object
-                Toolchain(CTOR toolchain_obj "${toolchain_contents}")
-
-                # Test that the toolchain map key changed accordingly
-                Toolchain(GET "${toolchain_obj}" toolchain_map toolchain_options)
-                cpp_map(GET "${toolchain_map}" value "test_prefix_path")
-                message(">>> test_prefix_path: ${value}")
-                ct_assert_equal(corr "${value}")
-            
-            endfunction()
-
-            #[[[
-            # Tests that a ``string(APPEND`` command is parsed properly.
-            #]]
-            ct_add_section(NAME "string_append_cmd")
-            function("${string_append_cmd}")
-
-                ct_add_section(NAME "single_line")
-                function("${single_line}")
-
-                    set(toolchain_contents "string(APPEND test_string \"a string\")\n")
-                    string(APPEND toolchain_contents "string(APPEND test_string \" with more appended\")")
-                    
-                    set(corr "a string with more appended")
-
-                    # Create a toolchain object
-                    Toolchain(CTOR toolchain_obj "${toolchain_contents}")
-
-                    # Test that the toolchain map key changed accordingly
-                    Toolchain(GET "${toolchain_obj}" toolchain_map toolchain_options)
-                    cpp_map(GET "${toolchain_map}" value "test_string")
-                    message(">>> test_string: ${value}")
-                    ct_assert_equal(corr "${value}")
-
-                endfunction()
-
-                ct_add_section(NAME "multi_line")
-                function("${multi_line}")
-
-                    set(toolchain_contents "string(APPEND\n test_string \n\"a st\nring\")\n")
-                    string(APPEND toolchain_contents "string(APPEND test_string \" with more appended\"\n)")
-                    
-                    set(corr "a st ring with more appended")
-
-                    # Create a toolchain object
-                    Toolchain(CTOR toolchain_obj "${toolchain_contents}")
-
-                    # Test that the toolchain map key changed accordingly
-                    Toolchain(GET "${toolchain_obj}" toolchain_map toolchain_options)
-                    cpp_map(GET "${toolchain_map}" value "test_string")
-                    message(">>> test_string: ${value}")
-                    ct_assert_equal(corr "${value}")
-
-                endfunction()
-            
-            endfunction()
-
-            #[[[
-            # Tests that the autopopulated values are overwritten and that
-            # new values are added.
-            #]]
-            ct_add_section(NAME "toolchain_content_string")
-            function("${toolchain_content_string}")
-
-                set(toolchain_contents "# Compilers
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
-
-set(PYTHON_EXECUTABLE python3)
-")
-
-                set(corr "{ \"CMAKE_C_COMPILER\" : \"gcc\", \"CMAKE_CXX_COMPILER\" : \"g++\", \"PYTHON_EXECUTABLE\" : \"python3\" }")
-
-                # Create a toolchain object with no user options
-                Toolchain(CTOR toolchain_obj "${toolchain_contents}")
-
-                # The file path should be blank
-                Toolchain(GET "${toolchain_obj}" my_file_path file_path)
-                ct_assert_equal(my_file_path "")
-
-                # Toolchain map should be equal to autopopulated options
-                Toolchain(GET "${toolchain_obj}" toolchain_map toolchain_options)
-                cpp_serialize(toolchain_map_serialized "${toolchain_map}")
-                message(">>> Toolchain map: ${toolchain_map_serialized}")
-                ct_assert_equal(corr "${toolchain_map_serialized}")
-            
-            endfunction()
-
-        endfunction()
-
-    endfunction()
-
-    #[[[
-    # Test creating a string for ``CMAKE_ARGS``.
-    #]]
-    ct_add_section(NAME "test_as_cmake_args")
-    function("${test_as_cmake_args}")
-        # include for cpp_decode_special_chars
-        include(cmakepp_lang/cmakepp_lang)
-
-#         set(toolchain_contents "# Compilers
-# set(CMAKE_C_COMPILER gcc)
-# set(CMAKE_CXX_COMPILER g++)
-
-# set(PYTHON_EXECUTABLE python3)
-# ")
-
-        set(toolchain_contents "set(CMAKE_C_COMPILER    gcc)
+            set(toolchain_contents "set(CMAKE_C_COMPILER    gcc)
 set(
     MPI_CXX_COMPILER
     /opt/openmpi-4.1.2/bin/mpicxx
@@ -255,25 +71,56 @@ list(APPEND CMAKE_PREFIX_PATH /usr/bin)
 list(APPEND CMAKE_PREFIX_PATH /usr/lib)
 string(APPEND 
     CMAKE_CXX_FLAGS 
-\" -GNinja\")
-string(APPEND\n test_string \n\"a st\nring\")")
+    \" -GNinja\")
+string(APPEND\n test_string \n\"a st\nring\")"
+            )
 
-        set(corr "-DCMAKE_C_COMPILER=\"gcc\" -DCMAKE_CXX_COMPILER=\"\" -DMPI_CXX_COMPILER=\"/opt/openmpi-4.1.2/bin/mpicxx\" -DMPI_C_COMPILER=\"/opt/openmpi-4.1.2/bin/mpicc\" -DFETCHCONTENT_SOURCE_DIR_CHEMCACHE=\"/home/zachcran/programs/chemcache\" -DCMAKE_PREFIX_PATH=\"/usr/bin\;/usr/lib\" -DCMAKE_CXX_FLAGS=\"-GNinja\" -Dtest_string=\"a st ring\"")
-        cpp_encode_special_chars("${corr}" corr)
+            cpp_encode_special_chars("${toolchain_contents}" encoded_toolchain)
 
-        # Create a toolchain object with no user options
+            # Create a toolchain object with the toolchain contents
+            Toolchain(CTOR toolchain_obj "${toolchain_contents}")
+
+            # The file content should match
+            Toolchain(GET "${toolchain_obj}" file_contents encoded_toolchain_contents)
+            ct_assert_equal(encoded_toolchain "${file_contents}")
+
+        endfunction()
+
+    endfunction()
+
+    ct_add_section(NAME "test_generate_file_contents")
+    function("${test_generate_file_contents}")
+
+        set(toolchain_contents "set(CMAKE_C_COMPILER    gcc)
+set(
+    MPI_CXX_COMPILER
+    /opt/openmpi-4.1.2/bin/mpicxx
+)
+set(unique_variable test)"
+        )
+
+        # Create a toolchain object with the toolchain contents
         Toolchain(CTOR toolchain_obj "${toolchain_contents}")
 
-        # The file path should be blank
-        Toolchain(GET "${toolchain_obj}" my_file_path file_path)
-        ct_assert_equal(my_file_path "")
+        Toolchain(generate_file_contents "${toolchain_obj}" file_contents)
 
-        # Toolchain map should be equal to autopopulated options
-        Toolchain(as_cmake_args "${toolchain_obj}" encoded_toolchain_args)
+        # Check for a few key phrases
 
-        message(">>> Toolchain args: ${encoded_toolchain_args}")
+        # Autopopulated options header
+        message("${file_contents}")
+        ct_assert_prints("# CMaize Autopopulated Options")
 
-        ct_assert_equal(corr "${encoded_toolchain_args}")
+        # CMAKE_CXX_COMPILER only set in autopopulated options
+        message("${file_contents}")
+        ct_assert_prints("CMAKE_CXX_COMPILER")
+
+        # User toolchain header
+        message("${file_contents}")
+        ct_assert_prints("# User Toolchain")
+
+        # unique_variable only in 
+        message("${file_contents}")
+        ct_assert_prints("unique_variable")
 
     endfunction()
 
