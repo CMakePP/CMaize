b9b6533feaa3944dd1a34e55a00577a7a8c9330b
zachcran <zachcran@gmail.com>
2022-05-09 14:31:05 -0500

    Create script to build api docs

---
+++ .github/workflows/scripts/build_docs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+
+# Start with a clean slate
+rm -rf docs/build
+rm -rf build/api_docs
+rm -rf docs/src/api/developer
+
+# Create a build directory for the api docs to be generated in
+mkdir -p build
+
+# Build the API docs with CMinx
+cminx "cmake/cmaize" -o "build/api_docs/" -r -p cmaize
+
+# Move the api docs to the documentation directory
+mv build/api_docs docs/src/api/developer
+
+# Build the Sphinx docs
+cd docs
+make html
