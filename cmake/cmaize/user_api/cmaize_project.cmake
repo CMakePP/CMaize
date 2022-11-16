c52f4b5e28f5b99912e2793a0fcf5d8dee921106
zachcran <zachcran@gmail.com>
2022-11-16 12:40:40 -0600

    Remove adding CMakePackageManager to the project immediately

--- cmake/cmaize/user_api/cmaize_project.cmake
+++ cmake/cmaize/user_api/cmaize_project.cmake
@@ -15,13 +15,6 @@ macro(cmaize_project _cp_name)
 
     CMaizeProject(CTOR _cp_project "${_cp_name}" ${ARGN})
 
-    # TODO: Turn this into a function that adds package managers as they
-    #       are encountered, instead of right at the beginning
-    CMaizeProject(GET "${_cp_project}" _cp_pms package_managers)
-    CMakePackageManager(CTOR _cp_cmake_pm)
-    list(APPEND _cp_pms "${_cp_cmake_pm}")
-    CMaizeProject(SET "${_cp_project}" package_managers "${_cp_pms}")
-
     cpp_set_global(CMAIZE_PROJECT_${PROJECT_NAME} "${_cp_project}")
 
 endmacro()
