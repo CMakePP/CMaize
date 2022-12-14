70fdf94106426c0138f3ea3ac820349ab47b74ac
zachcran <zachcran@gmail.com>
2022-12-14 15:44:53 -0600

    Fix version splitting function that was not setting return variables correctly

--- cmake/cmaize/utilities/split_version.cmake
+++ cmake/cmaize/utilities/split_version.cmake
@@ -24,28 +24,28 @@ include_guard()
 # :returns: ``tweak`` will be set to the fourth component of ``version``.
 # :rtype: str
 #]]
-function(cmaize_split_version major minor patch tweak version)
+function(cmaize_split_version _sv_major _sv_minor _sv_patch _sv_tweak _sv_version)
 
     # Split the version into components
-    string(REPLACE "." ";" version_comps "${version}")
+    string(REPLACE "." ";" _sv_version_comps "${_sv_version}")
 
-    list(LENGTH version_comps comp_count)
+    list(LENGTH _sv_version_comps _sv_comp_count)
 
-    if (comp_count GREATER_EQUAL 1)
-        list(GET version_comps 0 tmp_major)
-        set(major "${tmp_major}" PARENT_SCOPE)
+    if (_sv_comp_count GREATER_EQUAL 1)
+        list(GET _sv_version_comps 0 _sv_tmp_major)
+        set("${_sv_major}" "${_sv_tmp_major}" PARENT_SCOPE)
     endif()
-    if (comp_count GREATER_EQUAL 2)
-        list(GET version_comps 1 tmp_minor)
-        set(minor "${tmp_minor}" PARENT_SCOPE)
+    if (_sv_comp_count GREATER_EQUAL 2)
+        list(GET _sv_version_comps 1 _sv_tmp_minor)
+        set("${_sv_minor}" "${_sv_tmp_minor}" PARENT_SCOPE)
     endif()
-    if (comp_count GREATER_EQUAL 3)
-        list(GET version_comps 2 tmp_patch)
-        set(patch "${tmp_patch}" PARENT_SCOPE)
+    if (_sv_comp_count GREATER_EQUAL 3)
+        list(GET _sv_version_comps 2 _sv_tmp_patch)
+        set("${_sv_patch}" "${_sv_tmp_patch}" PARENT_SCOPE)
     endif()
-    if (comp_count GREATER_EQUAL 4)
-        list(GET version_comps 3 tmp_tweak)
-        set(tweak "${tmp_tweak}" PARENT_SCOPE)
+    if (_sv_comp_count GREATER_EQUAL 4)
+        list(GET _sv_version_comps 3 _sv_tmp_tweak)
+        set("${_sv_tweak}" "${_sv_tmp_tweak}" PARENT_SCOPE)
     endif()
 
 endfunction()
