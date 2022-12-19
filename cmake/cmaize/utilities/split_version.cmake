0097bdbfca08f09ce8662b7c5f5fb02e8cff1452
Zachery Crandall <zachcran@gmail.com>
2022-12-19 09:55:40 -0600

    Fix version splitting function (#91)
    * Fix version splitting function that was not setting return variables correctly
    
    * Update documentation

--- cmake/cmaize/utilities/split_version.cmake
+++ cmake/cmaize/utilities/split_version.cmake
@@ -1,51 +1,58 @@
 include_guard()
 
 #[[[
-# Split the given version string into its components. Some return values
-# may be blank.
+# Split a given version string into its components.
 #
-# :param major: Returned first component of the version.
-# :type major: str
-# :param minor: Returned second component of the version.
-# :type minor: str
-# :param patch: Returned third component of the version.
-# :type patch: str
-# :param tweak: Returned fourth component of the version.
-# :type tweak: str
-# :param version: Version string to be separated into components.
-# :type version: str
+# This function assumes that semantic versioning (`https://semver.org`__)
+# is being used, as well as an additional, optional TWEAK component that
+# CMake natively supports. Some return values may be blank if there are not
+# four components to the version string.
 #
-# :returns: ``major`` will be set to the first component of ``version``.
-# :rtype: str
-# :returns: ``minor`` will be set to the second component of ``version``.
-# :rtype: str
-# :returns: ``patch`` will be set to the third component of ``version``.
-# :rtype: str
-# :returns: ``tweak`` will be set to the fourth component of ``version``.
-# :rtype: str
+# :param _sv_major: Returned first component of the version.
+# :type _sv_major: desc
+# :param _sv_minor: Returned second component of the version.
+# :type _sv_minor: desc
+# :param _sv_patch: Returned third component of the version.
+# :type _sv_patch: desc
+# :param _sv_tweak: Returned fourth component of the version.
+# :type _sv_tweak: desc
+# :param _sv_version: Version string to be separated into components.
+# :type _sv_version: desc
+#
+# :returns: The first component of the given version.
+# :rtype: desc
+# :returns: The second component of the given version.
+# :rtype: desc
+# :returns: The third component of the given version.
+# :rtype: desc
+# :returns: The fourth component of the given version.
+# :rtype: desc
 #]]
-function(cmaize_split_version major minor patch tweak version)
+function(cmaize_split_version
+    _sv_major _sv_minor _sv_patch _sv_tweak _sv_version
+)
 
     # Split the version into components
-    string(REPLACE "." ";" version_comps "${version}")
+    string(REPLACE "." ";" _sv_version_comps "${_sv_version}")
 
-    list(LENGTH version_comps comp_count)
+    list(LENGTH _sv_version_comps _sv_comp_count)
 
-    if (comp_count GREATER_EQUAL 1)
-        list(GET version_comps 0 tmp_major)
-        set(major "${tmp_major}" PARENT_SCOPE)
+    # Extract each existing component
+    if (_sv_comp_count GREATER_EQUAL 1)
+        list(GET _sv_version_comps 0 _sv_tmp_major)
+        set("${_sv_major}" "${_sv_tmp_major}" PARENT_SCOPE)
     endif()
-    if (comp_count GREATER_EQUAL 2)
-        list(GET version_comps 1 tmp_minor)
-        set(minor "${tmp_minor}" PARENT_SCOPE)
+    if (_sv_comp_count GREATER_EQUAL 2)
+        list(GET _sv_version_comps 1 _sv_tmp_minor)
+        set("${_sv_minor}" "${_sv_tmp_minor}" PARENT_SCOPE)
     endif()
-    if (comp_count GREATER_EQUAL 3)
-        list(GET version_comps 2 tmp_patch)
-        set(patch "${tmp_patch}" PARENT_SCOPE)
+    if (_sv_comp_count GREATER_EQUAL 3)
+        list(GET _sv_version_comps 2 _sv_tmp_patch)
+        set("${_sv_patch}" "${_sv_tmp_patch}" PARENT_SCOPE)
     endif()
-    if (comp_count GREATER_EQUAL 4)
-        list(GET version_comps 3 tmp_tweak)
-        set(tweak "${tmp_tweak}" PARENT_SCOPE)
+    if (_sv_comp_count GREATER_EQUAL 4)
+        list(GET _sv_version_comps 3 _sv_tmp_tweak)
+        set("${_sv_tweak}" "${_sv_tmp_tweak}" PARENT_SCOPE)
     endif()
 
 endfunction()
