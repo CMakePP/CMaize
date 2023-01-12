89ba3a54be68121f4ae5388dedd518dff894567c
Zachery Crandall <zachcran@gmail.com>
2023-01-12 15:50:35 -0600

    Add output messages to clarify find or build dependency (#93)
    * Add output messages to clarify find or build dependency
    
    * Fix typo [skip ci]
    
    * Confirm message if find_package was successful
    
    * Output when the dependency is built successfully

--- cmake/cpp/user_api/find_or_build_dependency.cmake
+++ cmake/cpp/user_api/find_or_build_dependency.cmake
@@ -14,12 +14,22 @@ function(cpp_find_or_build_dependency _fobd_name)
         cpp_set_global("__CPP_DEPENDENCY_${_fobd_name}__" "${_fobd_depend}")
     endif()
 
+    message(STATUS "Attempting to find installed ${_fobd_name}")
+
     Dependency(FIND_DEPENDENCY "${_fobd_depend}" _fobd_found)
     if("${_fobd_found}")
+        message(STATUS "${_fobd_name} installation found")
         return()
     endif()
+    
+    message(STATUS "Attempting to fetch and build ${_fobd_name}")
 
     Dependency(BUILD_DEPENDENCY "${_fobd_depend}")
+
+    # The command above will throw build errors from inside FetchContent
+    # if the fetch and build fails, so we can assume at this point that
+    # it completed successfully
+    message(STATUS "${_fobd_name} build complete")
 
     # Alias the build target as the find_target to unify the API
     Dependency(GET "${_fobd_depend}" _fobd_find_target "find_target")
@@ -30,4 +40,5 @@ function(cpp_find_or_build_dependency _fobd_name)
         endif()
         add_library("${_fobd_find_target}" ALIAS "${_fobd_build_target}")
     endif()
+
 endfunction()
