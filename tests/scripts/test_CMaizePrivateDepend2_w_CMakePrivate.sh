00ac511a6e6b452d41f5f13eb5e2e9cbf34407a4
zachcran <zachcran@gmail.com>
2023-04-10 14:34:08 -0500

    Try combined tests

--- tests/scripts/test_CMaizePrivateDepend2_w_CMakePrivate.sh
+++ tests/scripts/test_CMaizePrivateDepend2_w_CMakePrivate.sh
@@ -3,5 +3,5 @@
 # Exit on error
 set -e
 
-source "./test_CMakePublic.sh $1"
-source "./test_CMaizePublicDepend2.sh $1"
+source "./test_CMakePublic.sh $1 $2"
+source "./test_CMaizePublicDepend2.sh $1 $2"
