022632ea0b036f3d25da54883a3128dd180fd0d3
zachcran <zachcran@gmail.com>
2023-04-14 14:05:33 -0500

    Remove double quotes stopping scripts from being 'source'ed

--- tests/scripts/test_CMaizePrivateDepend2_w_CMaizePrivateDepend.sh
+++ tests/scripts/test_CMaizePrivateDepend2_w_CMaizePrivateDepend.sh
@@ -3,5 +3,5 @@
 # Exit on error
 set -e
 
-source "./test_CMaizePrivateDepend.sh $1 $2"
-source "./test_CMaizePrivateDepend2.sh $1 $2"
+source ./test_CMaizePrivateDepend.sh $1 $2
+source ./test_CMaizePrivateDepend2.sh "" $2
--- tests/scripts/test_CMaizePrivateDepend2_w_all_deps.sh
+++ tests/scripts/test_CMaizePrivateDepend2_w_all_deps.sh
@@ -3,9 +3,6 @@
 # Exit on error
 set -e
 
-echo "pwd = `pwd`"
-echo "ls = `ls`"
-
-source "`pwd`/test_CMakePrivate.sh $1 $2"
-source "`pwd`/test_CMaizePrivateDepend.sh $1 $2"
-source "`pwd`/test_CMaizePrivateDepend2.sh $1 $2"
+source ./test_CMakePrivate.sh $1 $2
+source ./test_CMaizePrivateDepend.sh "" $2
+source ./test_CMaizePrivateDepend2.sh "" $2
