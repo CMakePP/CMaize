f852520293ce999bfd363c70be80740c0347382b
zachcran <zachcran@gmail.com>
2023-04-14 13:45:56 -0500

    Try pwd

--- tests/scripts/test_CMaizePrivateDepend2_w_all_deps.sh
+++ tests/scripts/test_CMaizePrivateDepend2_w_all_deps.sh
@@ -6,6 +6,6 @@ set -e
 echo "pwd = `pwd`"
 echo "ls = `ls`"
 
-source "./test_CMakePrivate.sh $1 $2"
-source "./test_CMaizePrivateDepend.sh $1 $2"
-source "./test_CMaizePrivateDepend2.sh $1 $2"
+source "`pwd`/test_CMakePrivate.sh $1 $2"
+source "`pwd`/test_CMaizePrivateDepend.sh $1 $2"
+source "`pwd`/test_CMaizePrivateDepend2.sh $1 $2"
