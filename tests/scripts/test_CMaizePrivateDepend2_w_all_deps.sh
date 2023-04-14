8af6f712cf47e337602ec12c5c1d958124241871
zachcran <zachcran@gmail.com>
2023-04-14 13:19:15 -0500

    Debug info

--- tests/scripts/test_CMaizePrivateDepend2_w_all_deps.sh
+++ tests/scripts/test_CMaizePrivateDepend2_w_all_deps.sh
@@ -3,6 +3,9 @@
 # Exit on error
 set -e
 
+echo "pwd = `pwd`"
+echo "ls = `ls`"
+
 source "./test_CMakePrivate.sh $1 $2"
 source "./test_CMaizePrivateDepend.sh $1 $2"
 source "./test_CMaizePrivateDepend2.sh $1 $2"
