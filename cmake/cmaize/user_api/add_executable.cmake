1488dabebe0f8032a864ba8254d4ed53ca473fe4
Zachery Crandall <zachcran@gmail.com>
2023-12-20 13:28:57 -0700

    Move RPATH and install path generation earlier in workflow (#145)
    * Initial tests passing
    
    * Remove missed debug statements
    
    * Add CMaizeProject test for duplicate target overwriting in the build target list

--- cmake/cmaize/user_api/add_executable.cmake
+++ cmake/cmaize/user_api/add_executable.cmake
@@ -66,6 +66,69 @@ function(cmaize_add_executable _cae_tgt_name)
     )
     CMaizeProject(add_language "${_cae_project}" "${_cae_LANGUAGE}")
 
+    # Build the install location for this library based on the project name.
+    # Note that this is overwritten if cmaize_find_or_build_dependency()
+    # is building this target.
+    cpp_get_global(_cae_proj CMAIZE_PROJECT_${PROJECT_NAME})
+    CMaizeProject(GET "${_cae_proj}" _cae_proj_name name)
+    set(
+        _cae_install_path
+        "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/${_cae_proj_name}"
+    )
+    CMaizeTarget(SET "${_cae_tgt_obj}" install_path "${_cae_install_path}")
+    CMaizeTarget(SET_PROPERTY
+        "${_cae_tgt_obj}"
+        INSTALL_PATH "${_cae_install_path}"
+    )
+
+    # Loop over each dependency. This is currently done by looking
+    # up the dependencies by name from the CMaizeProject, but later
+    # we should make each CMaize target hold references to its
+    # dependencies
+    cpp_get_global(_cae_top_proj CMAIZE_TOP_PROJECT)
+    BuildTarget(GET "${_cae_tgt_obj}" _dep_list depends)
+    foreach(dependency ${_dep_list})
+        # Fetch the dependency's target object
+        CMaizeProject(get_target
+            "${_cae_top_proj}" _dep_tgt_obj "${dependency}"
+        )
+
+        # Skip the dependency if it is not managed by CMaize, since
+        # those won't have install path information
+        if("${_dep_tgt_obj}" STREQUAL "")
+            continue()
+        endif()
+
+        # Get the install path for the dependency
+        CMaizeTarget(GET "${_dep_tgt_obj}" _dep_install_path install_path)
+
+        # Turn the dependency paths into absolute paths
+        file(REAL_PATH
+            "${_cae_destination_prefix}/${CMAKE_INSTALL_LIBDIR}/${_cae_pkg_name}"
+            _cae_lib_path
+            BASE_DIRECTORY "${CMAKE_INSTALL_PREFIX}"
+        )
+        # Replace install prefix with $ORIGIN
+        # We currently don't do this since we couldn't get it to work
+        # string(REPLACE "${_cae_lib_path}" "$ORIGIN" _dep_install_path "${_dep_install_path}")
+
+        # While not a great way to do it, this will check if the dependency
+        # has any INSTALL_RPATH data. If it does, we add that data to the
+        # current target as well. In theory, each target should manage its
+        # own INSTALL_RPATH set, but for now that does not seem to be working.
+        CMaizeTarget(has_property "${_dep_tgt_obj}" _dep_has_install_rpath INSTALL_RPATH)
+        if(_dep_has_install_rpath)
+            CMaizeTarget(get_property "${_dep_tgt_obj}" _dep_install_rpath INSTALL_RPATH)
+        endif()
+
+        # Actually append the aggregated paths to the current target's
+        # INSTALL_RPATH
+        CMaizeTarget(get_property "${_cae_tgt_obj}" _install_rpath INSTALL_RPATH)
+        list(APPEND _install_rpath ${_dep_install_path})
+        list(APPEND _install_rpath ${_dep_install_rpath})
+        CMaizeTarget(set_property "${_cae_tgt_obj}" INSTALL_RPATH "${_install_rpath}")
+    endforeach()
+
 endfunction()
 
 #[[[
