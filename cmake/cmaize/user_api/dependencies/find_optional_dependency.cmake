a745324f95a33e378e30b618bfa0e428df42dfcf
zachcran <zachcran@gmail.com>
2025-06-16 12:11:02 -0600

    NEEDS TESTING Replace find_optional_dependency implementation with modified find_or_build_optional_dependency rewrite

--- cmake/cmaize/user_api/dependencies/find_optional_dependency.cmake
+++ cmake/cmaize/user_api/dependencies/find_optional_dependency.cmake
@@ -59,11 +59,107 @@ function(cmaize_find_optional_dependency _cfod_name _cfod_flag)
 
     _check_optional_flag("${_cfod_flag}")
 
+    # Get the top CMaize project so we can look up the target that
+    # was just created from cmaize_find_or_build_dependency()
+    cpp_get_global(_cfod_top_proj_obj CMAIZE_TOP_PROJECT)
+
+    set(_cfod_tgt_obj "")
     if("${${_cfod_flag}}")
+        message(VERBOSE "\"${_cfod_name}\" enabled with ${_cfod_flag} = ${${_cfod_flag}}")
+
+        # Find or build the dependency target, adding it to the cmaize project.
         cmaize_find_dependency("${_cfod_name}" ${ARGN})
-        target_compile_definitions("${_cfod_name}" INTERFACE "${_cfod_flag}")
-    elseif(NOT TARGET "${_cfod_name}")
-        add_library("${_cfod_name}" INTERFACE)
+
+        # cmaize_find_dependency() raises PACKAGE_NOT_FOUND if dependency is
+        # not found, so no further checking if the target exists is needed
+
+        # Get the target that was found
+        CMaizeProject(get_target
+            "${_cfod_top_proj_obj}"
+            _cfod_tgt_obj
+            "${_cfod_name}"
+            INSTALLED
+        )
+
+        # CMaizeProject(get_target should never fail to find the target at
+        # this point.
+        # NOTE: This should be a cpp_assert call, but currently an empty string
+        # does not evaluate to false in cpp_assert, so an alternative was used.
+        if("${_cfod_tgt_obj}" STREQUAL "")
+            cpp_raise(TargetNotFound
+                "Target was not found in the project, but it should have been \
+                added by cmaize_find_or_build_dependency()!"
+            )
+        endif()
+
+        # Add the flag to the compile definitions of the dependency target.
+        # NOTE: It seems that 'target_compile_definitions' will not override
+        #       an existing definition, instead prepending the new definition:
+        #       https://gitlab.kitware.com/cmake/cmake/-/issues/24099.
+        #       To override an existing compile definition here, we would need
+        #       to check the INTERFACE_COMPILE_FEATURES target property and
+        #       replace the corresponding value if it exists.
+        CMaizeTarget(target "${_cfod_tgt_obj}" _cfod_tgt_name)
+        target_compile_definitions(
+            "${_cfod_tgt_name}" INTERFACE "${_cfod_flag}"
+        )
+    else()
+        message(VERBOSE "\"${_cfod_name}\" disabled with ${_cfod_flag} = ${${_cfod_flag}}")
+
+        # Even if the flag was not set, we still need a dummy target when the
+        # dependency is not included. This target should be set up such that 
+        # it is effectively a no-op when used during the build process later
+        # on, with its only purpose being to propagate the compile definition
+        # matching the fobod flag that toggles the target.
+        #
+        # This target should be defined on the CMaize side as well as CMake side:
+        #   CMaize side -> Create a CMaizeTarget object containing the name of the
+        #                  CMake target.
+        #   CMake side -> add_library("<target_name>" INTERFACE)
+
+        # TODO: Currently entries in the package manager are not being
+        # considered, nor is the dummy package being registered with it. Since
+        # that is typically where many of the kwargs are parsed, it is
+        # difficult to get the NAME value or the build or find target names
+        # without creating a Dependency object here.
+
+        # TODO: What happens when a package is disabled for a dependency, but
+        #       enabled in the top level package? However unlikely, it is a
+        #       state that should be handled here.
+
+        # Use the ALL flag to check both installed and built targets in the
+        # project, since an existing target may be stored under either.
+        CMaizeProject(check_target
+            "${_cfod_top_proj_obj}"
+            _cfod_tgt_found
+            "${_cfod_name}"
+            ALL
+        )
+
+        # If CMaize finds that the target is already defined for the project,
+        # just use that and move on. Someone has done our work for us already.
+        #
+        # Otherwise, we need to create it here.
+        if(NOT "${_cfod_tgt_found}")
+            # Wrap the target in a CMaizeTarget
+            CMaizeTarget(ctor
+                _cfod_tgt_obj
+                "${_cfod_name}"
+            )
+
+            # Add the dummy CMaize target to the project
+            # TODO: Should this be added as an INSTALLED target?
+            CMaizeProject(add_target
+                "${_cfod_top_proj_obj}"
+                "${_cfod_name}"
+                "${_cfod_tgt_obj}"
+            )
+
+            # Create the dummy CMake target if it doesn't exist yet
+            if(NOT TARGET "${_cfod_name}")
+                add_library("${_cfod_name}" INTERFACE)
+            endif()
+        endif()
     endif()
 
 endfunction()
